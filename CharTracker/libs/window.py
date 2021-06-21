f66153f866932899d9c4239ac4c06181f0119ddc
Ghureg <ghureglp@gmail.com>
2021-06-21 00:46:40 -0400

    Starting gui

---
+++ CharTracker/libs/window.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+import Char
+class dynamicActions(self, frame, acations):
+    def __init(self, master, name)
+class addChar(Frame):
+    def __init__(self, master, name, health, actions, ):
+        Frame.__init__(self, master, **options)
+
+if __name__=='__main__':
+    master=Tk()
+    master.configure(bg='#211c24')
+    master.title("Charcter Tracker")
+    mainFrame = ttk.Frame(master, padding="3 3 12 12")
+    mainFrame.grid(column=0,row=0, sticky=(N, W, E, S))
+    master.columnconfigure(0,weight=1)
+    master.rowconfigure(0, weight=1)
+    Characters = []
+    Char1 = Char.Character()
+    
+
+
